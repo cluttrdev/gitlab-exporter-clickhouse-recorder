190aa1156db5e6655781cd016561f443036f323b
cluttrdev <andreas@cluttr.tech>
2024-03-04 20:58:19 +0100

    chore: Update demo with readonly user

--- examples/demo/clickhouse/initdb.d/init-db.sh
+++ examples/demo/clickhouse/initdb.d/init-db.sh
@@ -1,18 +1,41 @@
-#!/bin/bash
+#!/bin/sh
 
-set -e
+set -eu
 
-database=${INITDB_CLICKHOUSE_DATABASE:-'gitlab_ci'}
-username=${INITDB_CLICKHOUSE_USER:-'default'}
-password=${INITDB_CLICKHOUSE_PASSWORD:-''}
+database=${INITDB_CLICKHOUSE_DATABASE}
+
+username=${INITDB_CLICKHOUSE_USER}
+password=${INITDB_CLICKHOUSE_PASSWORD}
+
+readonly_username=${INITDB_CLICKHOUSE_READONLY_USER}
+readonly_password=${INITDB_CLICKHOUSE_READONLY_PASSWORD}
 
 clickhouse client -n <<-EOSQL
+    /*
+    Create database
+    */
+
     CREATE DATABASE IF NOT EXISTS ${database};
+
+    /*
+    Create users
+    */
 
     CREATE USER IF NOT EXISTS ${username}
         IDENTIFIED WITH sha256_password BY '${password}'
         HOST ANY
         ;
 
+    CREATE USER IF NOT EXISTS ${readonly_username}
+        IDENTIFIED WITH sha256_password BY '${readonly_password}'
+        HOST ANY
+        ;
+    
+    /*
+    Manage access
+    */
+
     GRANT ALL ON ${database}.* TO ${username};
+
+    GRANT SELECT,SHOW ON ${database}.* TO ${readonly_username};
 EOSQL
